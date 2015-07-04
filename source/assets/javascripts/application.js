4384c1a8caeb3bb13581ec7b39614024c84fade1
John Wilson <john@j-wilson.com>
2015-07-04 14:44:25 -0400

    Add vidbg javascript and additional changes to hero to support said addition

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,3 +1,7 @@
+// --------------------------------------
+//  Navigation Toggle
+// --------------------------------------
+
 $(function() {
 
   $('.has-toggle').on('click', function() {
@@ -6,3 +10,17 @@ $(function() {
   });
 
 });
+
+// --------------------------------------
+//  Background Video
+//  -> References vendor.js
+// --------------------------------------
+
+var homepage = document.getElementsByClassName("index")[0];
+vidbg(homepage, [
+  {
+    src: 'assets/videos/placeholder.mp4',
+    type: 'mp4'
+  }
+], true);
+console.log(homepage);
---
+++ source/assets/javascripts/vendor.js
@@ -0,0 +1,30 @@
+// -------------------------------------
+//  Background Video
+// -------------------------------------
+
+/**
+ * @file vidbg is a 0.3kb micro-library for embedding video backgrounds into web pages.
+ * Scaffolded with generator-microjs v0.0.12
+ * @author Daniel Lamb <dlamb.open.source@gmail.com>
+ */
+
+function vidbg(element, sources, overlay) {
+  var container, item, html = [], index = sources.length;
+  if (!document.createElement('video').canPlayType) {
+    return;
+  }
+  html.push('<video autoplay="true" loop="loop" muted="muted" volume="0">');
+  sources.reverse();
+  while (index--) {
+    item = sources[index];
+    html.push('<source src="', item.src, '" type="video/', item.type, '" />');
+  }
+  html.push('</video>');
+  if (overlay) {
+    html.push('<div class="vidbg-overlay"></div>');
+  }
+  container = document.createElement('div');
+  container.setAttribute('class', 'vidbg');
+  container.innerHTML = html.join('');
+  element.appendChild(container);
+}
